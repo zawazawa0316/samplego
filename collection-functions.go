e5ef7e980f30334ff216edc1ddfb095ebfbcfc5d
zawazawa0316 <nightmare-before-christmas313@hotmail.co.jp>
2021-08-08 04:06:00 +0000

    up

--- collection-functions.go
+++ collection-functions.go
@@ -14,7 +14,7 @@ func Index(vs []string, t string) int {
 	return -1
 }
 
-func Include(vs []string, bool) {
+func Include(vs []string, t string) bool {
 	return Index(vs, t) >= 0
 }
 
@@ -27,7 +27,7 @@ func Any(vs []string, f func(string) bool) bool {
 	return false
 }
 
-func All (vs []string, f func(string) bool) bool {
+func All(vs []string, f func(string) bool) bool {
 	for _, v := range vs {
 		if !f(v) {
 			return false
@@ -40,7 +40,7 @@ func Filter(vs []string, f func(string) bool) []string {
 	vsf := make([]string, 0)
 	for _, v := range vs {
 		if f(v) {
-			vsf := append(vsf, v)
+			vsf = append(vsf, v)
 		}
 	}
 	return vsf
@@ -54,6 +54,24 @@ func Map(vs []string, f func(string) string) []string {
 	return vsm
 }
 
-func main () {
-	var strs = []string　
+func main() {
+	var strs = []string{"peach", "apple", "pear", "plum"}
+
+	fmt.Println(Index(strs, "pear"))
+
+	fmt.Println(Include(strs, "grape"))
+
+	fmt.Println(Any(strs, func(v string) bool {
+		return strings.HasPrefix(v, "p")
+	}))
+
+	fmt.Println(All(strs, func(v string) bool {
+		return strings.HasPrefix(v, "p")
+	}))
+
+	fmt.Println(Filter(strs, func(v string) bool {
+		return strings.Contains(v, "e")
+	}))
+
+	fmt.Println(Map(strs, strings.ToUpper))
 }
