5707b725d8d76e9f4cf8b1b1f11c386c201851ed
zawazawa0316 <nightmare-before-christmas313@hotmail.co.jp>
2021-08-15 07:44:10 +0000

    up

---
+++ command-line-arguments.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	argWithProg := os.Args
+	argWithoutProg := os.Args[1:]
+
+	arg := os.Args[3]
+
+	fmt.Println(argWithProg)
+	fmt.Println(argWithoutProg)
+	fmt.Println(arg)
+}
