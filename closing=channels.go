e8dea727afda425ec3d8d4341be659d554c192f6
zawazawa0316 <nightmare-before-christmas313@hotmail.co.jp>
2023-01-06 07:16:57 +0000

    12ファイルコミット

---
+++ closing=channels.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Println("sent job", j)
+		time.Sleep(time.Second)
+	}
+	close(jobs)
+	fmt.Println("sent all jobs")
+
+	<-done
+}
